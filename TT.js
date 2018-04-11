dae2ea679b52bf78c8784f67f62ab88f9db579e2
JulianDurdur <Juliandurdur@yahoo.com>
2018-04-11 18:14:56 +0200

    Initial release of sounds application.

---
+++ TT.js
@@ -0,0 +1,35 @@
+function boom(){
+    var boom = new Audio();
+    boom.src = "music/boom.wav"
+    boom.play()
+}
+
+function clap(){
+    var clap = new Audio();
+    clap.src = "music/clap.wav"
+    clap.play()
+}
+
+function kick(){
+    var kick = new Audio();
+    kick.src = "music/kick.wav"
+    kick.play()
+}
+
+function ride(){
+    var ride = new Audio();
+    ride.src = "music/ride.wav"
+    ride.play()
+}
+
+function tom(){
+    var tom = new Audio();
+    tom.src = "music/tom.wav"
+    tom.play()
+}
+
+function snare(){
+    var snare = new Audio();
+    snare.src = "music/snare.wav"
+    snare.play()
+}
